117ce84999b8884afe34abc7bcbdc74a2e41ac89
Arne <arne.limbourg@student.vives.be>
2025-12-29 13:15:51 +0100

    Export CSV aanpassing

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import argparse
-from app.cli import add_task, list_tasks, complete_task
+from app.cli import add_task, list_tasks, complete_task, export_csv
 
 parser = argparse.ArgumentParser(description="Task Manager CLI")
 parser.add_argument("--add", help="Add a new task")
 parser.add_argument("--list", action="store_true", help="List tasks")
 parser.add_argument("--done", type=int, help="Mark task as done")
+parser.add_argument("--export", action="store_true", help="Export to csv")
 
 args = parser.parse_args()
 
@@ -14,3 +15,5 @@ elif args.list:
     list_tasks()
 elif args.done:
     complete_task(args.done)
+elif args.export:
+    export_csv()
